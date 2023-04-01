07312fd3f0b147efa6a82d0027b360992a0f2962
migue <migue@LAPTOP-FKTUUEN1>
2023-04-01 13:59:51 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticaMVC.Models;
+
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext : DbContext
+    {
+        public equiposDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<marcas> marcas { get; set; }
+        public DbSet<estados_equipo> Estados_equipo { get; set; }
+        public DbSet<facultades> facultades { get; set;}
+        public DbSet<tipo_equipo> tipo_equipo { get; set;}
+        public DbSet<usuarios> usuarios { get; set; }
+        public DbSet<carreras> carreras { get; set; }
+
+    }
+}
